19beb1b87a56ff50432f3ef99c34bc421986aef9
Mummimasha <mummimasha92@gmail.com>
2022-01-02 18:33:55 +0300

    hanges to be committed

---
+++ HM2.js
@@ -0,0 +1,9 @@
+"use strict"
+let nameUser = 'John';
+let admin;
+admin = nameUser;
+alert ( admin );
+ let ourPlanet = 'Earth'  //Это мой вариант
+let ourPlanetName = "Земля";
+// а это правильный
+let currentUserName = 'John'
---
+++ hw3.js
@@ -0,0 +1,3 @@
+let userName = prompt ( "Как тебя зовут?","" );
+alert ( ` "Твоё имя" ${userName} ` );
+alert( "6" / "2" );
